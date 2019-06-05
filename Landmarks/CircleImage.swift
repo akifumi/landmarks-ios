fa79b7d00c8a403f2b696ac7384b542eb390af51
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-06-05 06:25:34 -0700

    Created CircleImage.swift

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by Akifumi Fukaya on 6/5/19.
+//  Copyright © 2019 Akifumi Fukaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct CircleImage : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct CircleImage_Previews : PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
+#endif
