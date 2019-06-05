3d7eb8c2f5d6a2a35d2f1d8637ec42f46e03624e
Akifumi Fukaya <akifumi.fukaya@gmail.com>
2019-06-05 09:57:31 -0700

    Created MapView.swift

---
+++ Landmarks/MapView.swift
@@ -0,0 +1,23 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Akifumi Fukaya on 6/5/19.
+//  Copyright © 2019 Akifumi Fukaya. All rights reserved.
+//
+
+import SwiftUI
+
+struct MapView : View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#if DEBUG
+struct MapView_Previews : PreviewProvider {
+    static var previews: some View {
+        MapView()
+    }
+}
+#endif
